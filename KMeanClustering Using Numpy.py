6c5aa3535dd8c84c7459b7a0048533e40ec4dd8d
eysha-raazia <eysharaazia@gmail.com>
2023-11-19 13:48:41 +0500

    New Dev

--- KMeanClustering Using Numpy.py
+++ KMeanClustering Using Numpy.py
@@ -8,7 +8,6 @@ class KMeanClustering:
     def __init__(self, k=3):
         self.meanPoints = None
         self.k = k
-        self.name = 'KMean Clustering'
         return
 
     def train(self, X):
